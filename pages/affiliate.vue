3b055680c6f60a07ea7c999af3096ac00478c768
danish2016 <danish.danish.danish2016@gmail.com>
2024-02-14 02:03:17 +0500

    Added media

--- pages/affiliate.vue
+++ pages/affiliate.vue
@@ -4,80 +4,84 @@
     <div class="bg-[#141416]">
       <div class="relative isolate">
         <div class="overflow-hidden">
-          <div class="mx-auto max-w-7xl px-6 pt-36 sm:pt-60 lg:px-8 lg:pt-10">
+          <div class="mx-auto max-w-7xl px-6 pt-10 md:pt-0 lg:px-8">
             <div
               class="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center"
             >
               <div class="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                 <h1
-                  class="text-4xl font-bold font-urban text-white sm:text-[81px] sm:leading-[1.1]"
+                  class="text-[56px] leading-[56px] font-bold text-center md:text-left font-urban text-white sm:text-[81px] sm:leading-[1.1]"
                 >
                   Ready to meet <br />
                   with your
 
                   <div class="inline-block">
                     <span> fans? </span>
-                    <svg
-                      width="165"
-                      height="14"
-                      viewBox="0 0 165 14"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M1 6.80053C43.7221 3.161 135.933 -1.09446 163 11"
-                        stroke="#FDC157"
-                        stroke-width="5"
-                      />
-                    </svg>
+                    <div class="w-[91px] h-[5px] md:w-[165px] md:h-[14px]">
+                      <svg
+                        viewBox="0 0 165 14"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M1 6.80053C43.7221 3.161 135.933 -1.09446 163 11"
+                          stroke="#FDC157"
+                          stroke-width="5"
+                        />
+                      </svg>
+                    </div>
                   </div>
                 </h1>
                 <p
-                  class="mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none"
+                  class="mt-6 text-lg text-center md:text-left leading-8 text-white sm:max-w-md lg:max-w-none"
                 >
-                  We help you build great meeting and easy-to schedule with your
-                  fans.
+                  We help you build great meeting and easy- <br />
+                  to schedule with your fans.
                 </p>
-                <div class="mt-10 flex items-center gap-x-6">
-                  <a
-                    href="#"
-                    class="rounded-full h-[64px] w-[198px] bg-primary flex items-center justify-center text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
-                    >Get started</a
+                <div
+                  class="mt-10 flex items-center justify-center md:justify-start"
+                >
+                  <button
+                    class="bg-primary px-[24px] flex items-center gap-4 shadow-xl shadow-primary/50 py-[16px] rounded-full"
                   >
+                    <span class="text-[18px] font-semibold">Get Started</span>
+                    <span class="p-1 text-white bg-black rounded-full">
+                      <ChevronRightIcon class="w-5 h-5" />
+                    </span>
+                  </button>
                 </div>
               </div>
+              <!-- sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0 -->
               <div
-                class="mt-14 flex justify-end items-end gap-4 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0"
+                class="flex justify-center md:justify-end items-end gap-4 -mt-20 md:mt-0 md:-mr-20"
               >
                 <div class="flex-none sm:ml-0 lg:order-last xl:order-none">
                   <div class="hero_img mb-[81px]">
-                    <img src="https://placehold.co/153x256" alt="" class="" />
+                    <img :src="hero_1" alt="" class="" />
                   </div>
                 </div>
                 <div class="flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
                   <div class="hero_img">
-                    <img
-                      src="https://placehold.co/153x281"
-                      alt=""
-                      class="!rounded-b-none"
-                    />
+                    <img :src="hero_2" alt="" class="" />
+                  </div>
+                  <div class="hero_img">
+                    <img :src="hero_3" alt="" class="" />
                   </div>
                 </div>
                 <div class="flex-none space-y-8 pt-32 sm:pt-0">
                   <div class="hero_img">
-                    <img src="https://placehold.co/153x402" alt="" class="" />
+                    <img :src="hero_4" alt="" class="" />
+                  </div>
+                  <div class="hero_img">
+                    <img :src="hero_5" alt="" class="" />
                   </div>
                 </div>
                 <div class="flex-none space-y-4 pt-32 sm:pt-0">
                   <div class="hero_img">
-                    <img src="https://placehold.co/153x402" alt="" class="" />
+                    <img :src="hero_6" alt="" class="" />
                   </div>
                   <div class="hero_img">
-                    <img
-                      src="https://placehold.co/153x224"
-                      alt=""
-                      class="!rounded-b-none"
-                    />
+                    <img :src="hero_7" alt="" class="" />
                   </div>
                 </div>
               </div>
@@ -95,26 +99,124 @@
       <div class="text-center font-sans font-semibold text-xl">
         Company Partner
       </div>
-      <div class="h-[16px] md:h-[30px] flex justify-between md:gap-x-[60px]">
-        <img src="https://placehold.co/100x30" alt="" />
-        <img src="https://placehold.co/100x30" alt="" />
-        <img src="https://placehold.co/100x30" alt="" />
-        <img src="https://placehold.co/100x30" alt="" />
-        <img src="https://placehold.co/100x30" alt="" />
+      <div
+        class="h-[16px] md:h-[30px] flex justify-between gap-x-0 md:gap-x-[60px]"
+      >
+        <img class="w-[51px] md:w-auto" :src="uber" alt="" />
+        <img class="w-[51px] md:w-auto" :src="google" alt="" />
+        <img class="w-[51px] md:w-auto" :src="paypal" alt="" />
+        <img class="w-[51px] md:w-auto" :src="microsoft" alt="" />
+        <img class="w-[51px] md:w-auto" :src="dribble" alt="" />
       </div>
     </div>
 
     <!-- Steps -->
-    <div>
-      <div>
-        <div>Steps to build your own audience and affiliate</div>
-        <div>
+    <div class="py-10 md:py-[100px]">
+      <div class="text-center">
+        <div class="mb-[10px] text-2xl md:text-5xl font-semibold">
+          Steps to build your own audience and affiliate
+        </div>
+        <div class="text-sm md:text-lg text-[#6B7280]">
           actually building an audience is not difficult, as long as you know
-          how, and this is an easy step to make it happen
+          how, and <br />
+          this is an easy step to make it happen
         </div>
       </div>
       <!-- Steps -->
-      <div class="flex"></div>
+      <div class="mt-10 max-w-7xl mx-auto flex justify-center flex-wrap">
+        <div
+          class="w-[162px] h-[175px] md:h-[250px] md:w-1/4 flex flex-col justify-between p-4 border-r-2"
+        >
+          <div class="w-8 md:w-12">
+            <svg
+              viewBox="0 0 48 48"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M43.4136 22.588L25.4576 4.58799C25.0828 4.2124 24.5741 4.001 24.0434 4.00025C23.5128 3.9995 23.0035 4.20946 22.6276 4.58399L4.58562 22.584C4.39937 22.7698 4.2516 22.9905 4.15077 23.2335C4.04995 23.4764 3.99805 23.7369 3.99805 24C3.99805 24.2631 4.04995 24.5235 4.15077 24.7665C4.2516 25.0095 4.39937 25.2302 4.58562 25.416L22.6276 43.416C23.4076 44.194 24.6796 44.192 25.4576 43.412L43.4136 25.412C43.5992 25.2266 43.7463 25.0065 43.8467 24.7642C43.9472 24.522 43.9989 24.2623 43.9989 24C43.9989 23.7377 43.9472 23.478 43.8467 23.2358C43.7463 22.9935 43.5992 22.7734 43.4136 22.588ZM29.9996 32H25.9996V24H19.9996V28L13.9996 22L19.9996 16V20H29.9996V32Z"
+                fill="#FDC157"
+              />
+            </svg>
+          </div>
+
+          <div class="text-sm font-semibold">
+            Choose the appropriate platform
+          </div>
+          <div class="text-xs md:text-sm text-[#777E90]">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </div>
+        </div>
+        <div
+          class="w-[162px] h-[175px] md:h-[250px] md:w-1/4 flex flex-col justify-between p-4 border-0 md:border-r-2"
+        >
+          <div class="w-8 md:w-12">
+            <svg
+              viewBox="0 0 49 48"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M36.666 18C36.666 15.794 34.872 14 32.666 14H29.798L24.962 5.942C24.6056 5.35105 24.103 4.8619 23.5025 4.52174C22.9021 4.18158 22.2241 4.00189 21.534 4H10.666V8H21.534L25.134 14H8.66602C6.46002 14 4.66602 15.794 4.66602 18V36C4.66602 38.206 6.46002 40 8.66602 40H32.666C34.872 40 36.666 38.206 36.666 36V30L44.666 34V20L36.666 24V18ZM22.666 34H10.666V30H22.666V34Z"
+                fill="#FDC157"
+              />
+            </svg>
+          </div>
+
+          <div class="text-sm font-semibold">Create content you like</div>
+          <div class="text-xs md:text-sm text-[#777E90]">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </div>
+        </div>
+        <div
+          class="w-[162px] h-[175px] md:h-[250px] md:w-1/4 flex flex-col justify-between p-4 border-r-2"
+        >
+          <div class="w-8 md:w-12">
+            <svg
+              viewBox="0 0 49 48"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M36.334 10.828L39.748 7.414L36.92 4.586L33.794 7.71C31.3 5.416 27.982 4 24.334 4C20.686 4 17.368 5.416 14.874 7.71L11.748 4.586L8.91998 7.414L12.334 10.828C11.0263 12.9918 10.3347 15.4718 10.334 18H6.33398V22H10.334V26H6.33398V30H10.334C10.334 37.72 16.616 44 24.334 44C32.052 44 38.334 37.72 38.334 30H42.334V26H38.334V22H42.334V18H38.334C38.3332 15.4718 37.6417 12.9918 36.334 10.828ZM30.334 30H18.334V26H30.334V30ZM30.334 22H18.334V18H30.334V22Z"
+                fill="#FDC157"
+              />
+            </svg>
+          </div>
+
+          <div class="text-sm font-semibold">Advertise your content</div>
+          <div class="text-xs md:text-sm text-[#777E90]">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </div>
+        </div>
+        <div
+          class="w-[162px] h-[175px] md:h-[250px] md:w-1/4 flex flex-col justify-between p-4"
+        >
+          <div class="w-8 md:w-12">
+            <svg
+              viewBox="0 0 48 48"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21.9993 35.832V40H17.9993V44H29.9993V40H25.9993V35.832C32.3233 35.028 37.6973 30.512 39.4253 24.246C39.9533 22.342 40.0493 20.186 39.7113 17.834L37.9793 5.716C37.911 5.2396 37.6732 4.80382 37.3095 4.48862C36.9458 4.17342 36.4806 3.99994 35.9993 4H11.9993C11.5181 3.99994 11.0529 4.17342 10.6892 4.48862C10.3255 4.80382 10.0877 5.2396 10.0193 5.716L8.28934 17.832C7.95134 20.186 8.04734 22.342 8.57534 24.244C10.3013 30.512 15.6753 35.028 21.9993 35.832ZM34.2653 8L35.4053 16H12.5913L13.7333 8H34.2653Z"
+                fill="#FDC157"
+              />
+            </svg>
+          </div>
+
+          <div class="text-sm font-semibold">
+            Your audience will be gathered
+          </div>
+          <div class="text-xs md:text-sm text-[#777E90]">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </div>
+        </div>
+      </div>
     </div>
 
     <div class="px-4 py-10 mx-auto max-w-7xl md:px-0">
@@ -123,7 +225,7 @@
       >
         <img
           :src="affiliate_1"
-          class="object-cover w-full h-full md:w-[40%] md:h-[70%]"
+          class="object-cover w-full h-full md:w-[30%] md:h-[70%]"
           alt=""
         />
         <div class="space-y-4 text-[48px] font-semibold">
@@ -180,7 +282,7 @@
                 <div class="text-base text-[#868491] font-medium">
                   Dommes Model
                 </div>
-                <div class="text-5xl font-bold">200+</div>
+                <div class="text-2xl md:text-5xl font-bold">200+</div>
               </div>
             </div>
             <div class="w-1/2 flex items-center gap-x-4 justify-center py-6">
@@ -217,26 +319,28 @@
                 <div class="text-base text-[#868491] font-medium">
                   Fans Gather
                 </div>
-                <div class="text-5xl font-bold">2300+</div>
+                <div class="text-2xl md:text-5xl font-bold">2300+</div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="flex flex-col items-center justify-between md:flex-row">
+      <div
+        class="mt-6 md:mt-0 flex flex-col items-center justify-between md:flex-row"
+      >
         <div class="space-y-4 text-[48px] font-semibold">
           <div>
-            <h2 class="">
+            <h2 class="text-2xl md:text-5xl text-[#323548] md:text-[#141416]">
               <span>What Makes Us Different?</span>
             </h2>
           </div>
-          <p class="text-[18px] text-[#6B7280] font-medium">
+          <p class="text-sm md:text-[18px] text-[#6B7280] font-medium">
             We are an agency that focuses on providing special training to
             become an influencer, starting from finding followers, building a
             brand, creating content, to becoming a great influencer.
           </p>
           <ul
-            class="flex flex-col gap-y-4 text-base text-[#0E0A23] font-normal"
+            class="flex flex-col gap-y-4 text-sm md:text-base text-[#0E0A23] font-normal"
           >
             <li class="h-10 flex items-center gap-x-4">
               <div>
@@ -505,18 +609,21 @@
       </div>
     </div>
 
-    <div class="max-w-7xl mx-auto">
+    <div class="px-10 md:p-0 max-w-7xl mx-auto">
       <LatestClips title="Blog Clips" />
     </div>
 
     <!-- creative -->
-    <div class="max-w-5xl py-24 mx-auto">
+    <div class="max-w-5xl py-10 md:py-24 mx-auto">
       <div class="mb-10 text-4xl font-bold text-center hidden md:block">
         Our Creative Dommes Say
       </div>
       <div class="flex flex-col md:flex-row gap-x-[120px]">
-        <!-- <img :src="creative_dommes" alt="" /> -->
-        <img src="https://placeholder.co/380x425" alt="" />
+        <img
+          :src="creative_dommes"
+          class="object-cover -ml-10 md:ml-0"
+          alt=""
+        />
         <div>
           <Splide
             ref="splide"
@@ -570,7 +677,9 @@
               </div></SplideSlide
             >
           </Splide>
-          <div class="flex justify-start gap-4">
+          <div
+            class="flex justify-center md:justify-start -mt-20 z-50 md:mt-0 gap-4"
+          >
             <button @click="slideToPrev" class="">
               <ArrowLeftIcon class="w-5 h-5" />
             </button>
@@ -594,7 +703,19 @@ import dommes from "../assets/imgs/dommes.png";
 import affiliate_1 from "../assets/imgs/affiliate_1.png";
 import creative_dommes from "../assets/imgs/creative_dommes.png";
 import { Splide, SplideSlide } from "@splidejs/vue-splide";
-
+import { ChevronRightIcon } from "@heroicons/vue/24/solid";
+import uber from "../assets/imgs/partner_logos/uber.png";
+import google from "../assets/imgs/partner_logos/google.png";
+import paypal from "../assets/imgs/partner_logos/paypal.png";
+import microsoft from "../assets/imgs/partner_logos/microsoft.png";
+import dribble from "../assets/imgs/partner_logos/dribble.png";
+import hero_1 from "../assets/imgs/affiliate_hero/hero_1.png";
+import hero_2 from "../assets/imgs/affiliate_hero/hero_2.png";
+import hero_3 from "../assets/imgs/affiliate_hero/hero_3.png";
+import hero_4 from "../assets/imgs/affiliate_hero/hero_4.png";
+import hero_5 from "../assets/imgs/affiliate_hero/hero_5.png";
+import hero_6 from "../assets/imgs/affiliate_hero/hero_6.png";
+import hero_7 from "../assets/imgs/affiliate_hero/hero_7.png";
 import logo from "../assets/imgs/logo-black.png";
 
 const splide = ref(null);
@@ -614,13 +735,13 @@ const mobileMenuOpen = ref(false);
   @apply relative;
 
   img {
-    @apply w-[153px] rounded-full bg-gray-900/5 object-cover shadow-lg;
+    @apply w-[72px] md:w-[153px] rounded-full bg-gray-900/5 object-cover shadow-lg;
   }
 }
 .creative_slide {
-  @apply h-full flex flex-col gap-y-14 pt-16;
+  @apply h-full w-1/2 md:w-full mx-auto flex flex-col gap-y-14 pt-16;
   .comment {
-    @apply text-2xl;
+    @apply text-base md:text-2xl;
   }
   .username {
     @apply font-bold;
